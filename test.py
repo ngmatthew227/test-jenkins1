654f81e4517701b20d0121c2ba5d8590ada454f5
matthew.ng <matthew.ng@2fi-solutions.com.hk>
2023-02-09 11:21:07 +0800

    test auto run

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ testString = "This is a test string"
 print(testString)
 testString = "This is a new test string"
 print(testString)
+testString = "This is a new new test string"
+print(testString)
+print("Test if the auto run works")
